95af216806e965b33affabb094a00f5bc644eafd
Yaniv Kessler <yanivk@gmail.com>
2022-10-18 08:41:20 +0300

    midflight

--- index.js
+++ index.js
@@ -1,74 +1,168 @@
 class FuzzySet {
-  constructor(mu, set) {
+  constructor(mu, crispSet) {
     this._mu = mu
-    this._set = set
+    this._crispSet = crispSet
   }
 
   // kernel
   core() {
-    return core(this._mu, this._set)
+    return core(this._mu, this._crispSet)
   }
 
   support() {
-    return support(this._mu, this._set)
+    return support(this._mu, this._crispSet)
   }
 
   height() {
-    return height(this._mu, this._set)
+    return height(this._mu, this._crispSet)
   }
 
   // alpha level set
   alphaCut(alpha) {
-    return alphaCut(this._mu, this._set, alpha)
+    return alphaCut(this._mu, this._crispSet, alpha)
   }
 
   // strong alpha is where MU(x) > alpha instead of >=
   strongAlphaCut(alpha) {
-    return strongAlphaCut(this._mu, this._set, alpha)
+    return strongAlphaCut(this._mu, this._crispSet, alpha)
   }
 
-  isNormalizedSet() {
-    return isNormalizedSet(this._mu, this._set)
+  isNormalized() {
+    return isNormalized(this._mu, this._crispSet)
+  }
+
+  isConvex() {
+    return isConvex(this._mu, this._crispSet[0], this._crispSet[this._crispSet - 1], 0.5)
   }
 }
 
-// kernel
-function core(MU, set) {
-  return set.filter(x => MU(x) === 1)
+// aka kernel
+function core(MU, crispSet) {
+  return alphaCut(MU, crispSet, 1)
 }
 
-function support(MU, set) {
-  return strongAlphaCut(MU, set, 0)
+// aka sup
+function support(MU, crispSet) {
+  return strongAlphaCut(MU, crispSet, 0)
 }
 
-function height(MU, set) {
+function height(MU, crispSet) {
   let max = -Infinity
-  for (const member of set) {
+  for (const member of crispSet) {
     max = Math.max(MU(member), max)
   }
   return max
 }
 
 // alpha level set
-function alphaCut(MU, set, alpha) {
+function alphaCut(MU, crispSet, alpha) {
   if (alpha < 0 || alpha > 1) {
     throw new TypeError('alpha must be between 0 and 1')
   }
-  return set.filter(x => MU(x) >= alpha)
+  return crispSet.filter(x => MU(x) >= alpha)
 }
 
 // strong alpha is where MU(x) > alpha instead of >=
-function strongAlphaCut(MU, set, alpha) {
+function strongAlphaCut(MU, crispSet, alpha) {
   if (alpha < 0 || alpha > 1) {
     throw new TypeError('alpha must be between 0 and 1')
   }
-  return set.filter(x => MU(x) > alpha)
+  return crispSet.filter(x => MU(x) > alpha)
 }
 
-function isNormalizedSet(MU, set) {
-  return height(MU, set) === 1
+function isNormalized(MU, crispSet) {
+  return height(MU, crispSet) === 1
 }
 
+function isConvex(MU, x1, x2, lambda) {
+  if (lambda < 0 || lambda > 1) {
+    throw new TypeError('lambda must be between 0 and 1')
+  }
+
+  // this should be a member of crispSet, but it's not??
+  // maybe member means, in the range of?
+  const crispConvex = lambda * x1 + ((1 - lambda) * x2)
+  // if (!crispSet.includes(crispConvex)) {
+  //   return false
+  // }
+  const muX1 = MU(x1)
+  const muX2 = MU(x2)
+  const muT = MU(crispConvex)
+  
+  return muT >= Math.min(muX1, muX2)
+}
+
+function isEqual(MUa, MUb, crispSet) {
+  for (const member of crispSet) {
+    if (MUa(member) !== MUb(member)) {
+      return false
+    }
+  }
+
+  return true
+}
+
+// A fuzzy subset of B if 
+// all MUa(x) <= MUb(x)
+function isSubset(MUa, MUb, crispSet) {
+  for (const member of crispSet) {
+    if (MUa(member) > MUb(x)) {
+      return false
+    }
+  }
+
+  return true
+}
+
+// A fuzzy subset of B if 
+// all MUa(x) < MUb(x)
+function isProperSubset(MUa, MUb, crispSet) {
+  for (const member of crispSet) {
+    if (MUa(member) >= MUb(x)) {
+      return false
+    }
+  }
+
+  return true
+}
+
+function scalarCardinality(MU, crispSet) {
+  return crispSet.reduce((prev, curr) => prev + MU(curr))
+}
+
+function relativeCardinality(MU, crispSet) {
+  const scalar = scalarCardinality(MU, crispSet) / crispSet.length
+}
+
+function fuzzyCardinality() {
+  // for each alpha (how do I select which alphas???)
+  // cadinality is simply the length (alpha is a crisp set)
+  // then the fuzzy cradinality is an ordered pair:
+  // {alphaCut(MU, crispSet, alpha).length, alpha}
+  // don't understand this yet...
+}
+
+// in classis set theory compliment is
+// X - universal set
+// A = {x memberOf X : some condition}
+// Ac (compliment) = {x memberOf X : x notMemberOf A}
+// maybe I can forgo the x member of X for Ac, but meh...
+//
+// in fuzzy set theory compliment is simply
+// a negation of MU; 1-MU(x)
+function compliment() {
+
+}
+
+function union(MUa, MUb, crispSet) {
+  // C = A ∪ B
+  // MUc = MAX(MUa(x), MUb(x)) for each member of crispSet
+}
+
+function intersection(MUa, MUb, crispSet) {
+  // C = A ∩ B
+  // MUc = MIN(MUa(x), MUb(x)) for each member of crispSet
+}
 
 module.exports = {
   core,
@@ -76,6 +170,10 @@ module.exports = {
   height,
   alphaCut,
   strongAlphaCut,
-  isNormalizedSet,
+  isNormalized,
+  isConvex,
+  isEqual,
+  isSubset,
+  isProperSubset,
   FuzzySet
 }
--- test.js
+++ test.js
@@ -6,7 +6,9 @@ const {
   height,
   alphaCut,
   strongAlphaCut,
-  isNormalizedSet
+  isConvex,
+  isEqual,
+  isNormalized
 } = require('./index')
 
 const MU = x => {
@@ -16,44 +18,66 @@ const MU = x => {
   return 0
 }
 
-const normalSet = [-1, 0, 1, 2, 3, 4, 5, 6]
-const outOfBoundsSet = [5, 6]
+const nonConvexMU = x => {
+  if (x === 2) return 1
+  if (x <= 9 && x > 6) return 0.5
 
+  return 0
+}
+
+const normalCrispSet = [-1, 0, 1, 2, 3, 4, 5, 6]
+const subNormalCrispSet = [5, 6]
 
 test('core', t => {
-  t.deepEqual(core(MU, normalSet), [2])
-  t.deepEqual(core(MU, outOfBoundsSet), [])
+  t.deepEqual(core(MU, normalCrispSet), [2])
+  t.deepEqual(core(MU, subNormalCrispSet), [])
 })
 
 test('support', t => {
-  t.deepEqual(support(MU, normalSet), [1, 2, 3])
-  t.deepEqual(support(MU, outOfBoundsSet), [])
+  t.deepEqual(support(MU, normalCrispSet), [1, 2, 3])
+  t.deepEqual(support(MU, subNormalCrispSet), [])
 })
 
 test('height', t => {
-  t.deepEqual(height(MU, normalSet), 1)
-  t.deepEqual(height(MU, outOfBoundsSet), 0)
+  t.deepEqual(height(MU, normalCrispSet), 1)
+  t.deepEqual(height(MU, subNormalCrispSet), 0)
 })
 
 test('alphaCut', t => {
-  t.deepEqual(alphaCut(MU, normalSet, 0), normalSet)
-  t.deepEqual(alphaCut(MU, normalSet, 0.5), [1, 2, 3])
-  t.deepEqual(alphaCut(MU, normalSet, 0.7), [2])
-  t.deepEqual(alphaCut(MU, outOfBoundsSet, 0.2), [])
-  t.deepEqual(alphaCut(MU, outOfBoundsSet, 0), outOfBoundsSet)
+  t.deepEqual(alphaCut(MU, normalCrispSet, 0), normalCrispSet)
+  t.deepEqual(alphaCut(MU, normalCrispSet, 0.5), [1, 2, 3])
+  t.deepEqual(alphaCut(MU, normalCrispSet, 0.7), [2])
+  t.deepEqual(alphaCut(MU, subNormalCrispSet, 0.2), [])
+  t.deepEqual(alphaCut(MU, subNormalCrispSet, 0), subNormalCrispSet)
 })
 
 test('strongAlphaCut', t => {
-  t.deepEqual(strongAlphaCut(MU, normalSet, 0), [1, 2, 3])
-  t.deepEqual(strongAlphaCut(MU, normalSet, 0.5), [2])
+  t.deepEqual(strongAlphaCut(MU, normalCrispSet, 0), [1, 2, 3])
+  t.deepEqual(strongAlphaCut(MU, normalCrispSet, 0.5), [2])
 
   // so strong alpha cut of 1 is always empty ??
   // since alpha can bet between 0 - 1 inclusive
-  t.deepEqual(strongAlphaCut(MU, normalSet, 1), [])
-  t.deepEqual(strongAlphaCut(MU, outOfBoundsSet, 0), [])
+  t.deepEqual(strongAlphaCut(MU, normalCrispSet, 1), [])
+  t.deepEqual(strongAlphaCut(MU, subNormalCrispSet, 0), [])
 })
 
-test('isNormalizedSet', t => {
-  t.true(isNormalizedSet(MU, normalSet))
-  t.false(isNormalizedSet(MU, outOfBoundsSet))
+test('isNormalized', t => {
+  t.true(isNormalized(MU, normalCrispSet))
+  t.false(isNormalized(MU, subNormalCrispSet))
+})
+
+test('isConvex', t => {
+  const aSet = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+  t.false(isConvex(nonConvexMU, 9, 2, 0.5))
+  t.true(isConvex(MU, 0, 2, 0.5))
+})
+
+test('isEqual', t => {
+  t.true(isEqual(MU, MU, normalCrispSet))
+  t.true(isEqual(MU, MU, [...normalCrispSet, 10, 8, 11]))
+  t.false(isEqual(nonConvexMU, MU, normalCrispSet))
+})
+
+test('isSubset', t => {
+
 })
