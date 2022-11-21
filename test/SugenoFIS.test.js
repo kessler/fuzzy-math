dfe6bb6d51afc4f7174c91667cd02745e0eae8d4
Yaniv Kessler <yanivk@gmail.com>
2022-11-21 15:42:52 +0200

    midflight

--- test/SugenoFIS.test.js
+++ test/SugenoFIS.test.js
@@ -16,8 +16,25 @@ test('SugenoFIS', t => {
     ]
   })
 
-  t.is(fis.evaluate({ x: 100, y: 25}), 350)
+  // rule 1
+  t.is(fis.evaluate({ x: 100, y: 25 }), 350)
 
   // rule 2
   t.is(fis.evaluate({ x: 37, y: 75 }), 317.5)
+})
+
+test('SugenoFIS - provide context for output msf that is not part of the antecedent', t => {
+  const fis = SugenoFIS.from({
+    sets: {
+      large: x => x >= 100 ? 1 : x >= 35 && x < 100 ? 0.5 : 0
+    },
+
+    rules: [
+      'if x is large and y is large then t'
+    ]
+  })
+
+  // rule 1
+  t.is(fis.evaluate({ x: 100, y: 100 }, { t: 100 }), 100)
+  t.throws(() => fis.evaluate({ x: 100, y: 100 }, { t: 100, y: 5 }), { message: 'context cannot have parameters that are part of the antecedent' })
 })
